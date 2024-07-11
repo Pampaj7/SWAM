6db2a19139926e73ba88bfad65d1c7c861b9c805
Pampaj7 <89346168+Pampaj7@users.noreply.github.com>
2024-07-11 11:39:08 +0200

    iris added

---
+++ iris.py
@@ -0,0 +1,14 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+iris = fetch_ucirepo(id=53) 
+  
+# data (as pandas dataframes) 
+X = iris.data.features 
+y = iris.data.targets 
+  
+# metadata 
+print(iris.metadata) 
+  
+# variable information 
+print(iris.variables) 
