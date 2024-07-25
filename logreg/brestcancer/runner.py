c11199e8292d32ced401e0a31694129e47771b73
Pampaj <leonardo.pamaploni666@gmail.com>
2024-07-26 00:41:28 +0200

    linker in logreg working

--- logreg/brestcancer/runner.py
+++ logreg/brestcancer/runner.py
@@ -1,4 +1,7 @@
+import subprocess
 import matlab.engine
+import lr_bc as lr
+
 
 def run_matlab_script():
     # chatgpt is dumb
@@ -7,4 +10,17 @@ def run_matlab_script():
     eng.quit()
 
 
+def run_r_script():
+    result = subprocess.run(["Rscript", "lr_bc.r"],
+                            capture_output=True, text=True)
+    print("R script output:")
+    print(result.stdout)
+    print(result.stderr)
+
+
+print("Running R script...")
+run_r_script()
+print("Running MATLAB script...")
 run_matlab_script()
+print("Running Python script...")
+lr.logreg()
