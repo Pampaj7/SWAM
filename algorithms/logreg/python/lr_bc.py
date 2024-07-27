bb03f9baffab8643f91282397d228cec65f6dafb
dvrkoo <73846636+dvrkoo@users.noreply.github.com>
2024-07-27 17:17:16 +0200

    .gitignore and folder refactoring

--- algorithms/logreg/python/lr_bc.py
+++ algorithms/logreg/python/lr_bc.py
@@ -3,16 +3,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
+
 def logreg():
-    csv_file_path = '../../datasets/breastcancer/breastcancer.csv'
+    csv_file_path = "../../datasets/breastcancer/breastcancer.csv"
     data = pd.read_csv(csv_file_path)
 
-    X = data.drop(columns=['diagnosis'])
-    y = data['diagnosis']
+    X = data.drop(columns=["diagnosis"])
+    y = data["diagnosis"]
 
-    y = y.map({'M': 1, 'B': 0})
+    y = y.map({"M": 1, "B": 0})
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     model = LogisticRegression(max_iter=10000)
 
--- algorithms/logreg/runner.py
+++ algorithms/logreg/runner.py
@@ -6,13 +6,12 @@ import lr_bc as lr
 def run_matlab_script():
     # chatgpt is dumb
     eng = matlab.engine.start_matlab()
-    eng.run('lr_bc.m', nargout=0)
+    eng.run("lr_bc.m", nargout=0)
     eng.quit()
 
 
 def run_r_script():
-    result = subprocess.run(["Rscript", "lr_bc.r"],
-                            capture_output=True, text=True)
+    result = subprocess.run(["Rscript", "lr_bc.r"], capture_output=True, text=True)
     print("R script output:")
     print(result.stdout)
     print(result.stderr)
