993e6ea08e871aba3b03a0c73a10e97ca0a23a1c
pippodima <pippodima99@gmail.com>
2024-08-23 18:01:41 +0200

    all working fine todo overwrite emissions.csv to every run

--- src/R/rRunnerSubProcess.py
+++ src/R/rRunnerSubProcess.py
@@ -1,5 +1,6 @@
 import subprocess
 from codecarbon import EmissionsTracker
+import pandas as pd
 
 
 def run_r_script(dataset, algorithm):
@@ -11,21 +12,43 @@ def run_r_script(dataset, algorithm):
 if __name__ == "__main__":
     datasets = ["breastCancer", "wine", "iris"]
     algorithms = ["logisticRegression", "XGBoost", "decisionTree", "randomForest", "KNN", "SVC", "GMM"]
+    repetition = 10
+    new_data = []
+    new_csv_filename = 'emissions_detailed.csv'  # Choose an appropriate name for the new file
 
     for dataset in datasets:
         for algorithm in algorithms:
-            tracker = EmissionsTracker()
+            for _ in range(repetition):
+                tracker = EmissionsTracker()
 
-            print("Executing R script:")
-            print(f"with {dataset} , {algorithm}")
+                print("Executing R script:")
+                print(f"with {dataset} , {algorithm}")
 
-            tracker.start()
+                tracker.start()
 
-            # Run the model and capture the result
-            result = run_r_script(dataset, algorithm)
+                # Run the model and capture the result
+                result = run_r_script(dataset, algorithm)
 
-            tracker.stop()
+                tracker.stop()
 
-            # Print the result
-            print("R function output:")
-            print(result)
+                # Print the result
+                print("R function output:")
+                print(result)
+
+                new_data.append({'Algorithm': algorithm, 'Dataset': dataset, 'Language': 'R'})
+
+    emissions_df = pd.read_csv('emissions.csv')
+    new_data_df = pd.DataFrame(new_data)
+    assert len(new_data_df) == len(emissions_df), "Mismatch in row count between emissions data and new columns."
+    emissions_df = pd.concat([emissions_df, new_data_df], axis=1)
+    emissions_df.to_csv(new_csv_filename, index=False)
+
+    print(f"{new_csv_filename} has been created with new columns.")
+
+
+
+
+
+
+
+
