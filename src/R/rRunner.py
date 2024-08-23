0d2678fb613a6b48b003de6cae137a80a81fa783
pippodima <pippodima99@gmail.com>
2024-08-23 18:38:21 +0200

    fixed bugs, now from allRunner.py all works fine todo: see the difference btw running from allRunner.py and rRunner.py

--- src/R/rRunner.py
+++ src/R/rRunner.py
@@ -9,17 +9,18 @@ def run_r_script(dataset, algorithm):
     return subResult
 
 
-if __name__ == "__main__":
+def mainR():
     datasets = ["breastCancer", "wine", "iris"]
     algorithms = ["logisticRegression", "XGBoost", "decisionTree", "randomForest", "KNN", "SVC", "GMM"]
-    repetition = 10
+    repetition = 1
     new_data = []
     new_csv_filename = 'emissions_detailed.csv'  # Choose an appropriate name for the new file
 
     for dataset in datasets:
         for algorithm in algorithms:
             for _ in range(repetition):
-                tracker = EmissionsTracker(output_dir='.', output_file="emissions.csv")
+
+                tracker = EmissionsTracker(output_dir='R', output_file="emissions.csv")
 
                 print("Executing R script:")
                 print(f"with {dataset} , {algorithm}")
@@ -37,18 +38,10 @@ if __name__ == "__main__":
 
                 new_data.append({'Algorithm': algorithm, 'Dataset': dataset, 'Language': 'R'})
 
-    emissions_df = pd.read_csv('emissions.csv')
+    emissions_df = pd.read_csv('R/emissions.csv')
     new_data_df = pd.DataFrame(new_data)
     assert len(new_data_df) == len(emissions_df), "Mismatch in row count between emissions data and new columns."
     emissions_df = pd.concat([emissions_df, new_data_df], axis=1)
-    emissions_df.to_csv(new_csv_filename, index=False)
+    emissions_df.to_csv(f'R/{new_csv_filename}', index=False)
 
     print(f"{new_csv_filename} has been created with new columns.")
-
-
-
-
-
-
-
-
--- src/allRunner.py
+++ src/allRunner.py
@@ -1,13 +1,11 @@
 import subprocess
+from R.rRunner import mainR
 # import matlab.engine
 from codecarbon import EmissionsTracker
 
 
 def run_r_script():
-    result = subprocess.run(["Rscript", "R/rRunner.R"], capture_output=True, text=True)
-    print("R script output:")
-    print(result.stdout)
-    print(result.stderr)
+   mainR()
 
 
 def run_python():
@@ -78,12 +76,9 @@ def run_cpp_program():
 
 
 if __name__ == "__main__":
-    tracker = EmissionsTracker()
 
     print("Esecuzione script R:")
-    tracker.start()
     run_r_script()
-    tracker.stop()
 
     # print("\nEsecuzione matlabRunner:")
     # tracker.start()
