8daa72c787cad5f8b87d9f3895098de731125fea
leona <leonardo.pampaloni1@edu.unifi.it>
2024-09-03 11:31:33 +0200

    added new plots to see correlations between feautures

--- src/newHandlePlotForFun.py
+++ src/newHandlePlotForFun.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
 
 
 def load_dataset(file_path):
@@ -22,9 +24,9 @@ def plot_execution_time_by_language_and_algorithm(df, language_col, algorithm_co
     plt.show()
 
 
-def plot_co2_distribution_by_language(df, language_col, co2_col, title="CO₂ Emissions Distribution by Language"):
+def plot_co2_distribution_by_language(df, language_col, co2_col, title="CO₂_Emissions_Distribution_by_Language"):
     plt.figure(figsize=(12, 8))
-    sns.boxplot(x=language_col, y=co2_col, data=df, palette='Set3')
+    sns.boxplot(x=language_col, y=co2_col, data=df, palette='Set3', hue='phase')
     plt.title(title)
     plt.xlabel('language')
     plt.ylabel('emissions')
@@ -32,7 +34,7 @@ def plot_co2_distribution_by_language(df, language_col, co2_col, title="CO₂ Em
     plt.show()
 
 
-def plot_duration_vs_emissions(df, duration_col, co2_col, title="Durata vs Emissioni di CO₂"):
+def plot_duration_vs_emissions(df, duration_col, co2_col, title="Durata_vs_Emissioni_di_CO₂"):
     plt.figure(figsize=(12, 8))
     sns.scatterplot(x=duration_col, y=co2_col, data=df, hue='language', palette='Set1')
     plt.title(title)
@@ -43,9 +45,9 @@ def plot_duration_vs_emissions(df, duration_col, co2_col, title="Durata vs Emiss
 
 
 def plot_co2_distribution_by_algorithm(df, algorithm_col, co2_col,
-                                       title="Distribuzione delle Emissioni di CO₂ per Algoritmo"):
+                                       title="Distribuzione_delle_Emissioni_di_CO₂_per_Algoritmo"):
     plt.figure(figsize=(12, 8))
-    sns.boxplot(x=algorithm_col, y=co2_col, data=df, palette='Set2')
+    sns.boxplot(x=algorithm_col, y=co2_col, data=df, palette='Set2', hue='phase')
     plt.title(title)
     plt.xlabel('Algoritmo')
     plt.ylabel('Emissioni di CO₂')
@@ -53,15 +55,15 @@ def plot_co2_distribution_by_algorithm(df, algorithm_col, co2_col,
     plt.show()
 
 
-def plot_comparison_by_phase(df, duration_col, co2_col, phase_col, title="Confronto tra Fasi (Test vs Train)"):
+def plot_comparison_by_phase(df, duration_col, co2_col, phase_col, title="Confronto_tra_Fasi_(Test_vs_Train)"):
     plt.figure(figsize=(12, 8))
-    sns.boxplot(x=phase_col, y=duration_col, data=df, palette='Set1')
+    sns.boxplot(x=phase_col, y=duration_col, data=df, palette='Set1', hue='language')
     plt.title(f"Distribuzione della Durata per {title}")
     plt.xlabel('Fase')
     plt.ylabel('Durata')
     plt.show()
     plt.figure(figsize=(12, 8))
-    sns.boxplot(x=phase_col, y=co2_col, data=df, palette='Set2')
+    sns.boxplot(x=phase_col, y=co2_col, data=df, palette='Set2', hue='language')
     plt.title(f"Distribuzione delle Emissioni di CO₂ per {title}")
     plt.xlabel('Fase')
     plt.ylabel('Emissioni di CO₂')
@@ -69,7 +71,7 @@ def plot_comparison_by_phase(df, duration_col, co2_col, phase_col, title="Confro
     plt.show()
 
 
-def plot_temporal_trends(df, time_col, duration_col, co2_col, title="Tendenze Temporali"):
+def plot_temporal_trends(df, time_col, duration_col, co2_col, title="Tendenze_Temporali"):
     df[time_col] = pd.to_datetime(df[time_col])
     df.sort_values(by=time_col, inplace=True)
     plt.figure(figsize=(12, 8))
@@ -84,7 +86,7 @@ def plot_temporal_trends(df, time_col, duration_col, co2_col, title="Tendenze Te
 
 
 def plot_duration_by_algorithm_and_dataset(df, algorithm_col, duration_col, dataset_col,
-                                           title="Durata per Algoritmo diviso per Dataset"):
+                                           title="Durata_per_Algoritmo_diviso_per_Dataset"):
     plt.figure(figsize=(14, 8))
     palette = sns.color_palette('husl', n_colors=df[dataset_col].nunique())
     sns.barplot(data=df, x=algorithm_col, y=duration_col, hue=dataset_col, palette=palette)
@@ -98,7 +100,7 @@ def plot_duration_by_algorithm_and_dataset(df, algorithm_col, duration_col, data
     plt.show()
 
 
-def plot_avg_co2_by_language(df, language_col, co2_col, title="Emissioni di CO₂ Medie per Linguaggio"):
+def plot_avg_co2_by_language(df, language_col, co2_col, title="Emissioni_di_CO₂_Medie_per_Linguaggio"):
     plt.figure(figsize=(12, 8))
     mean_co2 = df.groupby(language_col)[co2_col].mean()
     std_co2 = df.groupby(language_col)[co2_col].std()
@@ -113,7 +115,7 @@ def plot_avg_co2_by_language(df, language_col, co2_col, title="Emissioni di CO�
 
 
 def plot_performance_heatmap(df, language_col, algorithm_col, duration_col,
-                             title="Heatmap delle Performance per Linguaggio e Algoritmo"):
+                             title="Heatmap_delle_Performance_per_Linguaggio_e_Algoritmo"):
     pivot_table = df.pivot_table(values=duration_col, index=algorithm_col, columns=language_col, aggfunc='mean')
     plt.figure(figsize=(12, 8))
     sns.heatmap(pivot_table, annot=True, cmap='viridis', fmt=".2f")
@@ -125,19 +127,122 @@ def plot_performance_heatmap(df, language_col, algorithm_col, duration_col,
     plt.show()
 
 
+def plot_execution_time_by_dataset(df, dataset_col, time_col, language_col, title="Execution_Time_by_Dataset"):
+    plt.figure(figsize=(12, 8))
+    grouped_data = df.groupby([dataset_col, language_col])[time_col].sum().unstack()
+    grouped_data.plot(kind='bar', stacked=True, colormap='tab20', figsize=(12, 8))
+    plt.title(title)
+    plt.xlabel('Dataset')
+    plt.ylabel('Total Execution Time')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.savefig("graphics/" + title)
+    plt.show()
+
+
+def plot_execution_time_by_phase(df, dataset_col, time_col, phase_col, title="Execution_Time_by_Train_and_Test_Phases"):
+    plt.figure(figsize=(12, 8))
+
+    grouped_data = df.groupby([dataset_col, phase_col])[time_col].sum().unstack()
+    grouped_data.plot(kind='bar', stacked=False, colormap='Set2', figsize=(12, 8))
+
+    plt.title(title)
+    plt.xlabel('Dataset')
+    plt.ylabel('Total Execution Time')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig("graphics/" + title)
+    plt.show()
+
+
+def plot_correlation_cpu_energy_emissions(df, cpu_energy_col, emissions_col,
+                                          title="Correlation_between_CPU_Energy_and_Emissions"):
+    plt.figure(figsize=(10, 6))
+
+    # Scatter plot
+    sns.scatterplot(x=df[cpu_energy_col], y=df[emissions_col], color='blue', s=100)
+
+    # Linea di tendenza (trendline)
+    sns.regplot(x=df[cpu_energy_col], y=df[emissions_col], scatter=False, color='red', line_kws={"linewidth": 2})
+
+    plt.title(title)
+    plt.xlabel('CPU Energy (Joules)')
+    plt.ylabel('Emissions (CO₂)')
+
+    # Calcolo del coefficiente di correlazione
+    correlation = df[[cpu_energy_col, emissions_col]].corr().iloc[0, 1]
+    plt.annotate(f'Correlation: {correlation:.2f}', xy=(0.05, 0.95), xycoords='axes fraction', fontsize=12, color='red')
+
+    plt.tight_layout()
+    plt.savefig("graphics/" + title)
+    plt.show()
+
+
+def plot_multiple_correlations(df, columns, title="Multiple_Correlations"):
+    sns.pairplot(df[columns], kind='reg', plot_kws={'line_kws': {'color': 'red'}, 'scatter_kws': {'alpha': 0.5}})
+
+    plt.suptitle(title, y=1.02)  # Adjust the title position
+    plt.tight_layout()
+    plt.savefig("graphics/" + title)
+    plt.show()
+
+
+# we can use this function to remove outliers from the dataset and return a new dataset without outliers
+def remove_outliers(df, threshold=3):
+    # threshold (float): Il valore z-score sopra il quale un punto dati è considerato un outlier.
+
+    z_scores = np.abs(stats.zscore(df.select_dtypes(include=[np.number])))
+    mask = (z_scores < threshold).all(axis=1)
+    df_clean = df[mask]
+    return df_clean
+
+
+def benchmark_by_language(df, language_col, algorithm_col, dataset_col, metrics, title="Benchmark_by_Language"):
+    # Raggruppamento dei dati per linguaggio, algoritmo e dataset
+    grouped_df = df.groupby([language_col, algorithm_col, dataset_col])[metrics].mean().reset_index()
+
+    # Creazione del plot per ogni metrica
+    for metric in metrics:
+        plt.figure(figsize=(14, 8))
+        sns.barplot(x=algorithm_col, y=metric, hue=language_col, data=grouped_df, errorbar=None, palette="Set3")
+        plt.title(f"{title}: {metric.capitalize()}")
+        plt.xlabel('Algorithm')
+        plt.ylabel(metric.capitalize())
+        plt.legend(title='Language')
+        plt.savefig("graphics/" + metric + title)
+        plt.show()
+
+
+def plot_emissions_by_algorithm(df, algorithm_col, emissions_col, title="Emissions_by_Algorithm"):
+    # Calcolo delle emissioni medie per algoritmo
+    emissions_summary = df.groupby(algorithm_col)[emissions_col].mean().sort_values()
+
+    # Creazione del grafico a barre
+    plt.figure(figsize=(12, 8))
+    sns.barplot(x=emissions_summary.index, y=emissions_summary.values, palette='viridis')
+    plt.title(title)
+    plt.xlabel('Algorithm')
+    plt.ylabel('Average Emissions')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig("graphics/" + title)
+    plt.show()
+
+
 df = load_dataset('processedDatasets/meanEmissionsNew.csv')
 df_raw = load_dataset('raw_merged_emissions.csv')
+df_clean = remove_outliers(df)
 
-#u can exclude a language by choice
+# u can exclude a language by choice
 plot_execution_time_by_language_and_algorithm(df, 'language', 'algorithm', 'duration', 'phase', 'test',
-                                              title="Execution Time Distribution by Language and Algorithm (Test)",
+                                              title="Execution_Time_Distribution_by_Language_and_Algorithm_(Test)",
                                               excluded_language="java")
 
 plot_execution_time_by_language_and_algorithm(df, 'language', 'algorithm', 'duration', 'phase', 'train',
-                                              title="Execution Time Distribution by Language and Algorithm (Train)",
+                                              title="Execution_Time_Distribution_by_Language_and_Algorithm_(Train)",
                                               excluded_language="java")
 
-plot_co2_distribution_by_language(df, 'language', 'emissions', title="CO₂ Emissions Distribution by Language")
+plot_co2_distribution_by_language(df, 'language', 'emissions')
 
 plot_duration_vs_emissions(df, 'duration', 'emissions')
 
@@ -145,10 +250,28 @@ plot_co2_distribution_by_algorithm(df, 'algorithm', 'emissions')
 
 plot_comparison_by_phase(df, 'duration', 'emissions', 'phase')
 
-# need to refine
+# need to refine also use the original dataset with the timestamp!!!
 # plot_temporal_trends(df_raw, 'timestamp', 'duration', 'emissions')
 
 plot_duration_by_algorithm_and_dataset(df, 'algorithm', 'duration', 'dataset')
 
 plot_avg_co2_by_language(df, 'language', 'emissions')
 plot_performance_heatmap(df, 'language', 'algorithm', 'duration')
+
+plot_execution_time_by_dataset(df, 'dataset', 'duration', language_col='language')
+
+plot_execution_time_by_phase(df, 'dataset', 'duration', 'phase', )
+
+plot_correlation_cpu_energy_emissions(df, 'cpu_energy', 'emissions')
+
+# removed the noob outlier from nicco dumb pc
+columns_to_correlate = ['cpu_energy', 'emissions', 'energy_consumed', 'duration']
+plot_multiple_correlations(df_clean, columns_to_correlate)
+
+benchmark_by_language(df_clean,
+                      language_col='language',
+                      algorithm_col='algorithm',
+                      dataset_col='dataset',
+                      metrics=['duration', 'energy_consumed', 'emissions', 'cpu_energy', 'emissions_rate', 'cpu_power'])
+
+plot_emissions_by_algorithm(df, 'algorithm', 'emissions')
