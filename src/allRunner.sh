34e834b5a83c74fd1fe2db291f94557742e4a722
dvrkoo <73846636+dvrkoo@users.noreply.github.com>
2024-09-28 17:01:51 +0200

    AllRunner fixup

--- src/allRunner.sh
+++ src/allRunner.sh
@@ -1,19 +1,42 @@
-#this script runs all the project
+#!/bin/bash
 
-# this is the first enviroment used to run all the languages execpt c++
+echo "Activating the first environment (sw) for all languages except C++..."
 conda activate sw
+
+echo "Installing required Python packages from requirementsAll.txt..."
 pip install -r requirementsAll.txt
+
+echo "Running MATLAB and Python scripts using allRunner.py..."
 python allRunner.py
+
+echo "Navigating to the R directory and running rRunner.py..."
 cd R
 python3 rRunner.py
 cd ..
+
+echo "Navigating to the Java directory, building with Maven, and running jRunner.py..."
 cd java
+mvn clean install
 python jRunner.py
 cd ..
 
-#need th secondo enviroment to avoid conflicts with the first one
-# the requirements.txt file are provided
+echo "Switching to the C++ environment to avoid conflicts..."
 cd cpp
-conda cativate cpp
+conda activate cpp
+
+echo "Installing required Python packages from requirementsCpp.txt..."
 pip install -r requirementsCpp.txt
+
+echo "Running C++ related scripts with cRunner.py..."
 python cRunner.py
+
+
+cd ../
+
+echo "Creating Plots!"
+
+conda activate sw
+python handleCsv.py
+python HandlePlot.py
+echo "All tasks completed!"
+
